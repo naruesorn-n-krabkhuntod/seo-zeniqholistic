3c945a9a5066cba8a0e71287d54b25a1ad8a12e9
Naruesorn Krabkhuntod <155893454+naruesorn-n-krabkhuntod@users.noreply.github.com>
2025-06-04 18:41:34 +0700

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,6 @@
+้<header>
+    <img src="assets/image/logo_crop.webp" alt="">
+    <div class="menu">
+        <a href="/">หน้าหลัก</a>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Zeniq Aesthetic</title>
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+
+    <body>
+        <?php include("include/header.php");?>
+    </body>
+</html>
